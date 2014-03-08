f164fcf6539d6b2ce1ea823d77ece0449986d349
Matthieu Allegre <matthieu.allegre@gmail.com>
2014-03-08 21:44:38 +0100

    add validation, display errors in form

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ActiveRecord::Base
 
   LIST_NAMES = ['The Bedfathers','Bedzalcoatl']
 
+  validates :title, presence: true
+  validates :list, presence: true, inclusion: {in: 0.upto(LIST_NAMES.length-1)}
+  validates :points, presence: true, numericality: {only_integer: true}
+
+
   def list_name
     LIST_NAMES[list]
   end
