794df2aed318217ae70b206564b2951a08317b84
Matthieu Allegre <matthieu.allegre@gmail.com>
2014-03-18 00:00:14 +0100

    display admin name

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,11 @@
 class Admin < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :invitable, :database_authenticatable, :timeoutable,
          :trackable, :validatable
+
+
+  def name
+    email.split('@').first
+  end
+
 end
