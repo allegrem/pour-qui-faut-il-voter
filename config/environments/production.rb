ed156569ece2d5bc954b027307cd50f9596713ea
Matthieu Allegre <matthieu.allegre@gmail.com>
2014-03-18 00:28:38 +0100

    add smtp params

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ CampagneBed::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { :host => 'pour-qui-faut-il-voter.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => ENV['MAIL_ADDRESS'],
+    :port                 => ENV['MAIL_PORT'],
+    :domain               => ENV['MAIL_DOMAIN'],
+    :user_name            => ENV['MAIL_USERNAME'],
+    :password             => ENV['MAIL_PASSWORD'],
+    :authentication       => ENV['MAIL_AUTHENTICATION'],
+    :enable_starttls_auto => ENV['MAIL_STARTTLS']
+  }
 end
