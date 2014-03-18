ce8bb073ba5d90390596f7ccdd8456bb846ba944
Matthieu Allegre <matthieu.allegre@gmail.com>
2014-03-18 23:22:08 +0100

    update created_at field when challenge completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,9 @@ class ItemsController < ApplicationController
   # PATCH/PUT /items/1.json
   def update
     old_item = @item
+    if @item.challenge? && params[:list].to_i > -1
+      @item.created_at = DateTime.now
+    end
     respond_to do |format|
       if @item.update(item_params)
         Log.create admin: current_admin, item: @item, content: "Updated with params: list=#{@item.list_name} ; points=#{@item.points} ; title=#{@item.title}. Old params were: list=#{old_item.list_name} ; points=#{old_item.points} ; title=#{old_item.title}"
