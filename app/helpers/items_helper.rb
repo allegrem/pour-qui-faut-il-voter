c5f154f37dd665251c25d6ff1de941eaf875b077
Matthieu Allegre <matthieu.allegre@gmail.com>
2014-03-18 15:06:34 +0100

    add challenge option in items form

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
 
   def list_select_options
-    Item::LIST_NAMES.map.with_index { |l,i| [l,i] }
+    [["Challenge",-1]].concat Item::LIST_NAMES.map.with_index { |l,i| [l,i] }
   end
 
 end
